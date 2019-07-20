b706f4d035c4fe7434aaecde5ab17d7e9a4af2a5
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-07-20 13:53:09 -0400

    Fix indentation

--- ctf.c
+++ ctf.c
@@ -102,8 +102,8 @@ static int scan_test_names(int in_fd, char ***names, size_t *n_names,
 		size_t n_search = n_read + SHIFT_KEEP;
 		buf_space = sizeof(buf_stored) - SHIFT_KEEP;
 		if (wip_sym) {
-				search = buf;
-				n_search -= buf - buf_stored;
+			search = buf;
+			n_search -= buf - buf_stored;
 			char *end = memchr(search, '\n', n_search);
 			if (end) {
 				size_t n_added = end - search;
