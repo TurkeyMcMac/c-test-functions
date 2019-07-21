347dfdab9ab81b25f7d4e0ceb180a95de6ea125d
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-07-21 07:19:04 -0400

    Have tests return like main

--- libctf.h
+++ libctf.h
@@ -1,5 +1,3 @@
-struct ctf_context {};
-
 #define CTF_PREFIX Ctf_TEST___
 #define CTF_SUFFIX ___TEST_ctF
 #define CTF_CIRCUMFIX(name) CTF_CIRCUMFIX_(CTF_PREFIX, name, CTF_SUFFIX)
@@ -7,8 +5,8 @@ struct ctf_context {};
 #define CTF_CIRCUMFIX__(pfx, name, sfx) pfx##name##sfx
 
 #if CTF_TESTS_ENABLED
-#	define CTF_TEST(name, ...) void CTF_CIRCUMFIX(name) \
-		(struct ctf_context *ctf___context) { __VA_ARGS__; }
+#	define CTF_TEST(name, ...) int CTF_CIRCUMFIX(name)(void) \
+		{ __VA_ARGS__; return 0; }
 #else
 #	define CTF_TEST(name, ...)
 #endif
