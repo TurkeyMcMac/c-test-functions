3437bc27397744df0b1a2b6c44b685e4a7ee2e12
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-07-24 19:46:30 -0400

    Prevent C++ name mangling

--- lib/libctf.h
+++ lib/libctf.h
@@ -6,13 +6,22 @@
 #define CTF_CIRCUMFIX(name) CTF_CIRCUMFIX_(CTF_PREFIX, name, CTF_SUFFIX)
 #define CTF_CIRCUMFIX_(pfx, name, sfx) CTF_CIRCUMFIX__(pfx, name, sfx)
 #define CTF_CIRCUMFIX__(pfx, name, sfx) pfx##name##sfx
+#ifdef __cplusplus
+#	define CTF_EXTERN_C extern "C" {
+#	define CTF_END_EXTERN_C }
+#else
+#	define CTF_EXTERN_C
+#	define CTF_END_EXTERN_C
+#endif
 
 #if CTF_TESTS_ENABLED
 #	define CTF_TEST(name, ...) \
+	CTF_EXTERN_C \
 	/* Call the function name from the test hook so that assertion failures
 	 * report that function name: */ \
 	static int name(void) { __VA_ARGS__; return 0; } \
-	int CTF_CIRCUMFIX(name)(void) { return name(); }
+	int CTF_CIRCUMFIX(name)(void) { return name(); } \
+	CTF_END_EXTERN_C
 #else
 #	define CTF_TEST(name, ...)
 #endif
