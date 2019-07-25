85cb5b7bb59e15852982bfedea2ac58c4eceb954
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-07-25 07:55:08 -0400

    Move private libctf.h stuff to the file end

--- lib/libctf.h
+++ lib/libctf.h
@@ -1,25 +1,6 @@
 #ifndef LIBCTF_H_
 #define LIBCTF_H_
 
-#define CTF_PREFIX Ctf_TEST___
-#define CTF_SUFFIX ___TEST_ctF
-#define CTF_CIRCUMFIX(name) CTF_CIRCUMFIX_(CTF_PREFIX, name, CTF_SUFFIX)
-#define CTF_CIRCUMFIX_(pfx, name, sfx) CTF_CIRCUMFIX__(pfx, name, sfx)
-#define CTF_CIRCUMFIX__(pfx, name, sfx) pfx##name##sfx
-#ifdef __cplusplus
-#	define CTF_EXTERN_C extern "C" {
-#	define CTF_END_EXTERN_C }
-#else
-#	define CTF_EXTERN_C
-#	define CTF_END_EXTERN_C
-#endif
-/* To help protect against LTO when possible: */
-#ifdef __GNUC__
-#	define CTF_EXPORT __attribute__((used))
-#else
-#	define CTF_EXPORT
-#endif
-
 /* CTF_TEST(name, ...) makes a test function with the given name and a body
  * formed of the remaining arguments. This will only be present with the symbol
  * CTF_TESTS_ENABLED defined. The body fails if it crashes or returns nonzero.
@@ -37,6 +18,28 @@
 	CTF_END_EXTERN_C
 #else
 #	define CTF_TEST(name, ...)
+#endif
+
+#define CTF_PREFIX Ctf_TEST___
+#define CTF_SUFFIX ___TEST_ctF
+
+/* All that follows is not a public interface. */
+
+#define CTF_CIRCUMFIX(name) CTF_CIRCUMFIX_(CTF_PREFIX, name, CTF_SUFFIX)
+#define CTF_CIRCUMFIX_(pfx, name, sfx) CTF_CIRCUMFIX__(pfx, name, sfx)
+#define CTF_CIRCUMFIX__(pfx, name, sfx) pfx##name##sfx
+#ifdef __cplusplus
+#	define CTF_EXTERN_C extern "C" {
+#	define CTF_END_EXTERN_C }
+#else
+#	define CTF_EXTERN_C
+#	define CTF_END_EXTERN_C
+#endif
+/* To help protect against LTO when possible: */
+#ifdef __GNUC__
+#	define CTF_EXPORT __attribute__((used))
+#else
+#	define CTF_EXPORT
 #endif
 
 #endif /* LIBCTF_H_ */
