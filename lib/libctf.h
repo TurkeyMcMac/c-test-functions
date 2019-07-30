672f1d736a0d138e6c088c768a2b8847e3d68608
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-07-29 21:29:13 -0400

    Put extern "C" just before functions

--- lib/libctf.h
+++ lib/libctf.h
@@ -33,12 +33,10 @@
  */
 #if CTF_TESTS_ENABLED
 #	define CTF_TEST(name, ...) \
-	CTF_EXTERN_C \
 	/* Call the function name from the test hook so that assertion failures
 	 * report that function name: */ \
 	static int name(void) { __VA_ARGS__; return 0; } \
-	CTF_EXPORT int CTF_CIRCUMFIX(name)(void) { return name(); } \
-	CTF_END_EXTERN_C
+	CTF_EXTERN_C CTF_EXPORT int CTF_CIRCUMFIX(name)(void) { return name(); }
 #else
 #	define CTF_TEST(name, ...)
 #endif
@@ -54,11 +52,9 @@
 #define CTF_CIRCUMFIX_(pfx, name, sfx) CTF_CIRCUMFIX__(pfx, name, sfx)
 #define CTF_CIRCUMFIX__(pfx, name, sfx) pfx##name##sfx
 #ifdef __cplusplus
-#	define CTF_EXTERN_C extern "C" {
-#	define CTF_END_EXTERN_C }
+#	define CTF_EXTERN_C extern "C"
 #else
 #	define CTF_EXTERN_C
-#	define CTF_END_EXTERN_C
 #endif
 /* To help protect against LTO when possible: */
 #ifdef __GNUC__
