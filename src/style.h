e2f2b82b9ebce800cd98243505b6fa9f30c0eab9
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-08-04 20:55:32 -0400

    Document style API

--- src/style.h
+++ src/style.h
@@ -3,14 +3,20 @@
 
 #include <stdbool.h>
 
+/* Set styles on or off, overriding the default that depends on whether the
+ * output is a tty. */
 void set_styles(bool on);
 
+/* Returns the bold style start if styles are on. */
 const char *style_bold(void);
 
+/* Returns the red foreground style start if styles are on. */
 const char *style_fg_red(void);
 
+/* Returns the green foreground style start if styles are on. */
 const char *style_fg_green(void);
 
+/* Returns the code to end all styles if styles are on. */
 const char *style_end_all(void);
 
 #endif /* STYLE_H_ */
