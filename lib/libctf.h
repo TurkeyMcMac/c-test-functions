52593c47d8fc968aa1dc7d8ccfaa71e6eccef7d5
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-03-19 09:10:13 -0400

    Add ANSI C interface

--- lib/libctf.h
+++ lib/libctf.h
@@ -24,6 +24,10 @@
 #ifndef LIBCTF_H_
 #define LIBCTF_H_
 
+#include <libc89tf.h>
+
+/* This is now a wrapper around libc89tf.h with minor convenience added. */
+
 /* CTF_TEST(name, ...) makes a test function with the given name and a body
  * formed of the remaining arguments. This will only be present with the symbol
  * CTF_TESTS_ENABLED defined. The body fails if it crashes or returns nonzero.
@@ -32,35 +36,9 @@
  * the test symbols must be present for the test runner to pick up on the tests.
  */
 #if CTF_TESTS_ENABLED
-#	define CTF_TEST(name, ...) \
-	/* Call the function name from the test hook so that assertion failures
-	 * report that function name: */ \
-	static int name(void) { __VA_ARGS__; return 0; } \
-	CTF_EXTERN_C CTF_EXPORT int CTF_CIRCUMFIX(name)(void) { return name(); }
+#	define CTF_TEST(name, ...) CTF_TEST_FUN(name) { __VA_ARGS__; return 0; }
 #else
 #	define CTF_TEST(name, ...)
-#endif
-
-/* The part of a test symbol before the identifier. */
-#define CTF_PREFIX CT9f6
-/* The part of a test symbol after the identifier. */
-#define CTF_SUFFIX Ct5F_
-
-/* All that follows is not a public interface. */
-
-#define CTF_CIRCUMFIX(name) CTF_CIRCUMFIX_(CTF_PREFIX, name, CTF_SUFFIX)
-#define CTF_CIRCUMFIX_(pfx, name, sfx) CTF_CIRCUMFIX_2(pfx, name, sfx)
-#define CTF_CIRCUMFIX_2(pfx, name, sfx) pfx##name##sfx
-#ifdef __cplusplus
-#	define CTF_EXTERN_C extern "C"
-#else
-#	define CTF_EXTERN_C
-#endif
-/* To help protect against LTO when possible: */
-#ifdef __GNUC__
-#	define CTF_EXPORT __attribute__((used))
-#else
-#	define CTF_EXPORT
 #endif
 
 #endif /* LIBCTF_H_ */
